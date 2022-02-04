56aa747115902338babe68bb44b63f4b51c01c0f
ouailagzenay <ouailagzenay@gmail.com>
2022-02-04 11:47:28 +0100

    up

---
+++ les variable/demo.php
@@ -0,0 +1,9 @@
+<?php 
+$nome = 'ouail' ;
+$prenon = 'agzenay' ;
+$note1= 10 ;
+$note2 = 30 ;
+$totale = 20 ;
+echo 'bounjeur ' . $nome .' '. $prenon . ' vous avez eu ' .(($note1+$note2)/2). ' de mouyane';
+echo  "\nbounjeur $nome $prenon vous avez eu $totale de mouyane" ;
+?>
---
+++ table/demo.php
@@ -0,0 +1,20 @@
+<?php
+$student = [ 10, 20 ,30 , [' merradou' , ' mtake' , 'dilige']];
+$classe = [
+    [
+        'name' => 'fouade',
+        'id' => 'P14984784',
+        'note' => '15'
+    ],
+    [
+        'name' => 'ni3ma',
+        'id' => 'P1498654"',
+        'note' => '18'
+    ]
+    ];
+
+echo $classe[1]['id'][0];
+
+?>
+
+ 
