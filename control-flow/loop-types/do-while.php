704cafa27d18e973f5835d747f250bce752ff1d5
ouailagzenay <ouailagzenay@gmail.com>
2022-02-04 12:35:14 +0100

    up

---
+++ control-flow/loop-types/do-while.php
@@ -0,0 +1,8 @@
+<?php
+$x = 1;
+do{
+    echo "the number is : $x <br>";
+    $x++;
+
+}while($x <= 6);
+?>
---
+++ get-post/get/get.php
@@ -0,0 +1,5 @@
+<?php 
+if($_GET['parametre1']){
+    echo "welcome " . $_GET['parametre1']."<br/>";
+};
+?>
