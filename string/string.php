db4565e4b941bd7aa2f5bcbdc7fc13b6e60b7252
ouailagzenay <ouailagzenay@gmail.com>
2022-02-14 13:14:41 +0100

    up

---
+++ string/string.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<body>
+
+<?php
+echo strlen("Hello world!"); //outpout : 12
+?> 
+<br>
+<?php
+echo str_word_count("Hello world!");//outpout : 2
+?> 
+<br>
+<?php
+echo str_replace("world", "Dolly", "Hello world!"); //outpout : Hello Dolly!
+?> 
+
+</body>
+</html>
