348b3d52b22b4fe2f2bb54ced9cd23d9e72bf538
ouailagzenay <ouailagzenay@gmail.com>
2022-02-14 12:57:24 +0100

    up

---
+++ array/exemple1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php
+    $voiture = array('volvol', 'BMW' ,'toyota');
+    echo "I'lilk ". $voiture[0]. " and " , $voiture[1] ." , " .$voiture[2];
+    ?>
+</body>
+</html>
