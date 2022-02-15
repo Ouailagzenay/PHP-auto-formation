c6b18a4bd595ad74eb237c1430cb6f42ef2470f4
ouailagzenay <ouailagzenay@gmail.com>
2022-02-15 15:44:51 +0100

    suprime

--- Form Introduction/index.php
+++ Form Introduction/index.php
@@ -1,16 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<body>
-    <form action="login.php" method="post">
-        <label for="username">Name :</label>
-        <input type="text" name="username" id="usernameInput" required>
-        <label for="password">password :</label>
-        <input type="password" name="password"  id="passwordInput" required>
-        <button type="submit">Login</button>
-    </form>
-    
-</body>
-</html> -->
 <!DOCTYPE html>
 <html lang="en">
 <body>
