87dbf5c2f137b0ede2d16fa538418ba99526333e
ouailagzenay <ouailagzenay@gmail.com>
2022-02-15 13:22:05 +0100

    up

---
+++ Session/exemple1.php
@@ -0,0 +1,11 @@
+<?php session_start();?>
+<html>
+  <body>
+    <form action="exemple2.php" method="post">
+    <input type="text" name="name" >
+    <input type="number" name="age">
+    <input type="submit" name="butten"  value="login">
+   </form>   
+  </body>
+
+</html>
---
+++ Session/exemple2.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST["name"];
+$age = $_POST["age"];
+
+if (!empty($_POST['butten'])){
+    echo 'your name : '. $_SESSION['name'] = $name;
+    echo 'your age : '. $_SESSION['age'] = $age ;
+}
+?>
